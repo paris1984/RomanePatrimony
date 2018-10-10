b692ed24bc39c3fa1a6c5898ce67537b0b9ffea0
darkparis <jlmartinr1984@gmail.com>
2018-10-10 21:32:18 +0200

    inicio de pantalla de detalle

---
+++ app/src/test/java/jlmartin/es/romanepatrimony/MunicipiosSqlCreateDbHelperTest.java
@@ -0,0 +1,31 @@
+package jlmartin.es.romanepatrimony;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import jlmartin.es.romanepatrimony.entidad.Municipio;
+import jlmartin.es.romanepatrimony.sql.MunicipiosSqlCreateDbHelper;
+
+public class MunicipiosSqlCreateDbHelperTest {
+
+    @Test
+    public void getMunicipiosTest() throws IOException {
+
+
+        InputStream input = new FileInputStream("src\\test\\java\\jlmartin\\es\\romanepatrimony\\data_municipios.dat");
+        List<Municipio> municipios = MunicipiosSqlCreateDbHelper.creation(input);
+        Assert.assertNotNull(municipios);
+        Assert.assertFalse(municipios.isEmpty());
+        for(Municipio municipio:municipios){
+            Assert.assertNotNull(municipio.getLatitud());
+            Assert.assertNotNull(municipio.getLongitud());
+            Assert.assertNotNull(municipio.getNombre());
+            Assert.assertNotNull(municipio.getProvincia_id());
+        }
+    }
+}
